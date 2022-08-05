f5037dc9edda28257cf96e41517130a595c9fe59
ashiikkhan <ashiksust06@gmail.com>
2022-08-05 17:31:15 +0600

    switching sections done

--- app.js
+++ app.js
@@ -15,18 +15,42 @@ function pageTransition() {
       this.className += ' active-btn';
     });
   }
+  //  sections active class:
+  allSections.addEventListener('click', (e) => {
+    const id = e.target.dataset.id;
+    if (id) {
+      sectBtns.forEach((btn) => {
+        btn.classList.remove('active');
+      });
+      e.target.classList.add('active');
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      });
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
 }
 pageTransition();
 
-// another way to remove and add active btn :
+// another ways to remove and add active btn :
+
+// for (let i = 0; i < sectBtn.length; i++) {
+//   sectBtn[i].addEventListener('click', function () {
+//     let currentBtn = document.querySelectorAll('.active-btn');
+//     currentBtn[0].classList.remove('active-btn');
+//     sectBtn[i].classList.add('active-btn');
+//   });
+// }
+// pageTransition();
 
 // function pageTransition() {
-//   for (let i = 0; i < sectBtn.length; i++) {
-//     sectBtn[i].addEventListener('click', function () {
-//       let currentBtn = document.querySelectorAll('.active-btn');
-//       currentBtn[0].classList.remove('active-btn');
-//       sectBtn[i].classList.add('active-btn');
+//   sectBtn.forEach((button) => {
+//     button.addEventListener('click', function () {
+//       let currentBtn = document.querySelector('.active-btn');
+//       currentBtn.classList.remove('active-btn');
+//       button.classList.add('active-btn');
 //     });
-//   }
+//   });
 // }
 // pageTransition();
